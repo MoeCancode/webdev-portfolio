ec16678b860768822d1a6ea64c521117ed62c1f0
M A Razvi <abuzarflw@gmail.com>
2022-04-17 23:48:32 -0700

    Add carousel and populate it wiht projects and images

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-// $("#aboutPicture").css("height", "2000px");
-
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
         }
-    })
-  });
-
+    }
+})
